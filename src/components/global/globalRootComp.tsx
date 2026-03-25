444338b9aeeeaa5542b99b026a8961011d8f2700
Abhimanyu6676 <iamlive24@gmail.com>
2026-03-25 03:41:07 +0000

    --

--- src/components/global/globalRootComp.tsx
+++ src/components/global/globalRootComp.tsx
@@ -6,7 +6,7 @@ import { NotificationContainer } from "../alert/notificationContainer";
 import { TabSync } from "../auth/TabSync";
 // these imports are supposed to be imported at last
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/global.scss";
+import "../../styles/global.scss";
 //
 export const GlobalRootComp: React.FC<{ element: ReactNode }> = ({ element }) => {
   return (
