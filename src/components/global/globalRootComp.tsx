432a323e0ce98d61267bcccfb330b0c939cf7be7
Abhimanyu6676 <iamlive24@gmail.com>
2026-03-31 07:43:03 +0000

    - initiated eventBus with new `initiate` function instead of old `_constructor`

--- src/components/global/globalRootComp.tsx
+++ src/components/global/globalRootComp.tsx
@@ -1,15 +1,15 @@
 import React, { ReactNode, useEffect } from "react";
 import ReduxWrapper from "../../redux/reduxWrapper";
-import eventBus from "../../util/eventBus";
 import { SocketContainer } from "../../services/socket/socketContainer";
+import eventBus from "../../util/eventBus";
+import Alert from "../alert";
 import { NotificationContainer } from "../alert/notificationContainer";
 import { TabSync } from "../auth/TabSync";
-import DataEventsListenerClass from "./dataEventsListener";
 import CrudEventsListenerClass from "./crudEventsListener";
+import DataEventsListenerClass from "./dataEventsListener";
 // these imports are supposed to be imported at last
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/global.scss";
-import Alert from "../alert";
 //
 export const GlobalRootComp: React.FC<{ element: ReactNode }> = ({ element }) => {
   return (
@@ -29,7 +29,7 @@ const InitiateClasses = () => {
   const initiateClasses = () => {
     const isBrowser = typeof window !== undefined;
     if (isBrowser) {
-      eventBus._constructor();
+      eventBus.initiate();
       DataEventsListenerClass.initiate();
       CrudEventsListenerClass.initiate();
       Alert.initiate();
