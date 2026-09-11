bace170d000b7adef246a1dd04b6fa6a538246cb
Abhimanyu6676 <37015707+Abhimanyu6676@users.noreply.github.com>
2026-09-12 00:48:45 +0530

    --7

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -2,15 +2,15 @@ echo "==========================================================================
 
 echo "removing existing '/var/www/trade' & '~/trading/trade/public' folder"
 
-rm -rf /var/www/trade
-rm -rf public
+#rm -rf /var/www/trade
+#rm -rf public
 
 # Load Node.js environment (uncomment/modify if using nvm)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 npm run build
 
 echo "Copying build html output directory to '/var/www/trade'"
 
-cp -r public /var/www/trade
+#cp -r public /var/www/trade
