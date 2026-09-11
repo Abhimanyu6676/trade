507ae549ea6ea509a9ffb578daca9c165752b786
Abhimanyu6676 <37015707+Abhimanyu6676@users.noreply.github.com>
2026-09-12 00:27:19 +0530

    --1

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -1,3 +1,3 @@
-rm -rf /var/www/trade
-mkdir /var/www/trade
-echo "This is a single line of text.." > /var/www/trade/index.html
+sudo rm -rf /var/www/trade
+sudo mkdir /var/www/trade
+sudo echo "This is a single line of text.." > /var/www/trade/index.html
