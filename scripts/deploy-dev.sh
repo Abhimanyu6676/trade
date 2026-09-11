6e78699cd25cb5a580bbe573f040c439fcbf825f
Abhimanyu6676 <37015707+Abhimanyu6676@users.noreply.github.com>
2026-09-12 00:36:24 +0530

    --4

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -1,8 +1,13 @@
+echo "==========================================================================================="
+
 echo "removing existing '/var/www/trade' & '~/trading/trade/public' folder"
 
 rm -rf /var/www/trade
 ls
-
+echo "==========================================================================================="
+rm -rf public
+echo "==========================================================================================="
+ls
 
 mkdir /var/www/trade
 echo "This is a single line of text.." > /var/www/trade/index.html
